dd4dc8031f6e5f46cd660e7e3b484da3e0c4c30b
Brunopuod <80692024+Brundex@users.noreply.github.com>
2024-03-31 21:19:11 -0300

    Update passport.js

--- src/config/passport/passport.js
+++ src/config/passport/passport.js
@@ -1,6 +1,7 @@
 import local from 'passport-local'
 import passport from 'passport'
 import { userModel } from '../../models/user.js'
+import GithubStrategy from 'passport-github2'
 import { createHash, validatePassword } from '../../utils/bcrypt.js'
 
 const localStrategy = local.Strategy
@@ -43,6 +44,26 @@ const initializePassport = () => {
         }
     }))
 
+    passport.use('github', new GithubStrategy({
+        clientID: "",
+        clientSecret: "",
+        callbackURL: "http://localhost:8000/api/session/githubSession"
+    }, async (accessToken, refreshToken, profile, done) => {
+        try {
+            const user = await userModel.findOne({ email: profile._json.email }).lean()
+            if (user) {
+                done(null, user)
+            } else {
+                const randomNumber = crypto.randomUUID()
+                console.log(profile._json)
+                const userCreated = await userModel.create({ first_name: profile._json.name, last_name: ' ', email: profile._json.email, age: 18, password: createHash(`${profile._json.name}`) })
+                console.log(randomNumber)
+                return done(null, userCreated)
+            }
+        } catch (error) {
+            return done(error)
+        }
+    }))
 
 
 
